238d3de5c488e3cd4a40b93ec2fdedc4257bd817
Vova Lytvyn <lytvynvova1@gmail.com>
2021-07-06 15:12:01 +0300

    Create task0.py

---
+++ task0.py
@@ -0,0 +1,21 @@
+import random
+
+
+print("\n\nA list of 30 random integers from -100 to +100:")
+array = range(-100, 100)
+numbers = random.sample(array, 30)
+print(numbers)
+
+
+print("\nMaximum list item:")
+print(max(numbers))
+
+
+print("\nSequence number of the maximum list item:")
+print(numbers.index(max(numbers)))
+
+
+print("\nPairs of negative numbers standing side by side:\n")
+for i in range(len(numbers)-1):
+    if numbers[i] < 0 and numbers[i+1] < 0:
+        print(numbers[i], numbers[i+1])
